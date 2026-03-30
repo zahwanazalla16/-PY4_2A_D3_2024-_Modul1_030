9d5c67c0033630dc6f99f70863aeef66eb4d3633
zahwanazalla16 <zahwa.nazalla@gmail.com>
2026-03-30 08:13:37 +0700

    kembalikan counter

--- lib/features/counter/counter_controller.dart
+++ lib/features/counter/counter_controller.dart
@@ -1,14 +1,11 @@
-import 'package:hive_flutter/hive_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CounterController {
   int _counter = 0;
   int _step = 1;
 
-  late Box<dynamic> _counterBox;
-  late Future<void> initFuture;
-
   final List<String> _history = [];
-  
+
   int get value => _counter;
   int get step => _step;
   List<String> get history => List.unmodifiable(_history);
@@ -23,42 +20,49 @@ class CounterController {
   }
 
   // load data saat app dibuka 
-  void loadData() {
-    _counter = _counterBox.get('counter_value') ?? 0;
-    _step = _counterBox.get('counter_step') ?? 1;
+  Future<void> loadData(String username) async {
+    final prefs = await SharedPreferences.getInstance();
 
-    final savedHistory = _counterBox.get('counter_history');
+    _counter = prefs.getInt('counter_value_$username') ?? 0;
+    _step = prefs.getInt('counter_step_$username') ?? 1;
+
+    final savedHistory =
+        prefs.getStringList('counter_history_$username');
     if (savedHistory != null) {
       _history.clear();
-      _history.addAll(List<String>.from(savedHistory));
+      _history.addAll(savedHistory);
     }
   }
 
   // simpan data
-  void _saveData() {
-    _counterBox.put('counter_value', _counter);
-    _counterBox.put('counter_step', _step);
-    _counterBox.put('counter_history', _history);
+  Future<void> _saveData(String username) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    await prefs.setInt('counter_value_$username', _counter);
+    await prefs.setInt('counter_step_$username', _step);
+    await prefs.setStringList(
+        'counter_history_$username', _history);
   }
 
-  void _addHistory(String text) {
+  void _addHistory(String username, String text) {
     _history.insert(0, text);
     if (_history.length > 5) {
       _history.removeLast();
     }
-    _saveData();
+    _saveData(username);
   }
 
-  void setStep(int value) {
+  void setStep(int value, String username) {
     if (isValidStep(value)) {
       _step = value;
-      _saveData();
+      _saveData(username);
     }
   }
 
   void increment(String username) {
     _counter += _step;
     _addHistory(
+      username,
       '${_currentTime()} user $username menambahkan $_step menjadi $_counter',
     );
   }
@@ -66,6 +70,7 @@ class CounterController {
   void decrement(String username) {
     _counter -= _step;
     _addHistory(
+      username,
       '${_currentTime()} user $username mengurangi $_step menjadi $_counter',
     );
   }
@@ -74,16 +79,8 @@ class CounterController {
     _counter = 0;
     _step = 1;
     _addHistory(
+      username,
       '${_currentTime()} user $username mereset nilai counter',
     );
-  }
-
-  CounterController(String username) {
-    initFuture = _init(username);
-  }
-
-  Future<void> _init(String username) async {
-    _counterBox = await Hive.openBox<dynamic>('counter_${username}_box');
-    loadData();
   }
 }
